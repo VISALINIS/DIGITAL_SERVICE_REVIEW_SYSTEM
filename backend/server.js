27bdeaeaede77993f3929f4d6e86f3f10afae614
Visalini S <svisalini0@gmail.com>
2026-04-03 20:32:09 +0530

    fix frontend build path

--- backend/server.js
+++ backend/server.js
@@ -32,10 +32,10 @@ app.use("/api/admin", adminRoutes);
 //frontendconnection
 const path = require("path");
 
-app.use(express.static(path.join(__dirname, "../client/dist")));
+app.use(express.static(path.join(__dirname, "../client/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 const PORT = process.env.PORT || 5000;
